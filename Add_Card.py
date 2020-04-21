c3407f41ece5732cb7526874d63b43f3b285445c
Fahad Sheikh <fahadsheikh3010@gmail.com>
2020-04-21 08:40:31 +0530

    Automation#3

--- Add_Card.py
+++ Add_Card.py
@@ -1,9 +1,12 @@
 import requests
+import issues
+
+name = issues.titles
 url = "https://api.trello.com/1/cards"
 
 query = {
 
-'name':'trialgit#2',
+'name':name,
 'idList':'5e95c3a80d601f5535eb4259',
 'key':'2227ef7694cce98f830eb80a7d545ef7',
 'token':'03341061d27ae05bb27b3e1c82beffb7b1472bd580c9b0631401773f2f007c80'
---
+++ issues.py
@@ -0,0 +1,11 @@
+import requests
+url = "https://api.github.com/repos/fahad-30/Snake.py/issues"
+
+response = requests.request("GET",url)
+
+# print(response.text)
+response= response.json()
+
+dict1 = response[0]
+titles = dict1["title"] 
+
